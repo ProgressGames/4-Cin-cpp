c879b7057f0d9324b2aa183c93a7e47571681d25
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-08-12 16:44:37 +0300

    Add file cin

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int Var;
+
+
+	cout << "Введите число:";
+	cin >> Var;
+	cout << "Вы ввели число:" << Var;
+}
